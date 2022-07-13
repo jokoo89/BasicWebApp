bf2a60add61f782771ab13bece87b024bc393d09
Konstantin Johl <konstantin.johl@tum.de>
2022-07-13 12:38:03 +0200

    Task1

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,9 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else { //
+            //hello World
+            // TODO extend the programm here
             return "";
         }
     }

8c0c3514f4b50731e0e7245749149ea58cb6d4b8
Konstantin Johl <konstantin.johl@tum.de>
2022-07-13 13:51:00 +0200

    Added Plus - new try 2

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "konstantin";
-        } else if (query.contains("plus")) {
+        } /*else if (query.contains("plus")) {
             //query = "what is 9 plus 15"
             String[] wordList = query.split(" "); //nach jedem Leerzeichen werden die Strings separiert ->DAadruch
             int sum = 0;
@@ -25,8 +25,13 @@ public class QueryProcessor {
                 } catch(Exception e){
                     //
             }
-                return String.valueOf(sum);
-        }
+                return String.valueOf(sum);*/
+            } else if (query.contains("plus")) {
+                String[] wordList = query.split(" ");
+                String wordList1 = wordList[3];
+                String wordList2 = wordList[5];
+                return String.valueOf(Integer.parseInt(wordList1) + Integer.parseInt(wordList2));
+            }
            // Integer.parseInt("myString 2");
             //query.length(); //für >10
             //String nine = query.substring(8, 1);
